053cbc66533b56441241983ff0bd050acecc1ac4
Pradyumna Gayake <prgayake100@gmail.com>
2022-08-18 12:06:51 +0530

    Updated Complaint Registration

--- routes/user/userComplaints.js
+++ routes/user/userComplaints.js
@@ -2,8 +2,10 @@ const express = require('express');
 const isLoggedIn = require('../../middleware');
 const router = express.Router();
 
-
-router.post('/LodgeComplaint',isLoggedIn, (req, res) => {
+router.get('/RegisterComplaint', (req, res) => {
+    res.render('user/complaintRegistration');
+})
+router.post('/LodgeComplaint', (req, res) => {
     res.json(req.body);
 })
 
