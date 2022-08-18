b77dedf5d5a59806d05588cd5486297242668226
niranjangirhe <niranjangirheindia@gmail.com>
2022-08-18 19:48:59 +0530

    updated usercomplaint insert function call

--- routes/user/userComplaints.js
+++ routes/user/userComplaints.js
@@ -18,14 +18,7 @@ router.post('/LodgeComplaint',isLoggedIn, (req, res) => {
         Mobile: {S:req.body.Mobile},
         ComplaintBody: {S:req.body.ComplaintBody}
     }
-<<<<<<< HEAD
     crud.Insert(complaintData, 'complaints', res.session.user.idToken.payload.sub);
-=======
-    crud.Insert(complaintData)
-    console.log(req.body);
-    // upload.uploadFilestoS3(req.files.docs)
-    
->>>>>>> e0077618a790ab49a6a5a1d949f931f004909424
 })
 
 
