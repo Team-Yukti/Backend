3a233dfaad4e787688c8515449dc1b3b45f1d6bd
niranjangirhe <niranjangirheindia@gmail.com>
2022-08-20 09:16:02 +0530

    Firebase key added

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ const path = require('path');
 const { sendfile } = require('express/lib/response');
 var app = express();
 const isLoggedIn = require('./middleware');
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+
+
+const crud = require('./routes/crud.js');
+const serviceAccount = require('./keys/firebase.json');
+
+initializeApp({
+  credential: cert(serviceAccount)
+});
+
+const db = getFirestore();
+
+
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -45,3 +59,4 @@ app.get('/ForgotPassword', (req, res) => {
     res.render('auth/forgotPassword');
 })
 app.listen(3000, function () { console.log('Example app listening on port 3000!');});
+
