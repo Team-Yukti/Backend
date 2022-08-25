b0071999d69c1d3205ca83c033df12c4b9c76fdd
niranjangirhe <niranjangirheindia@gmail.com>
2022-08-25 10:21:39 +0530

    SE for complaint page updated

--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -381,63 +381,6 @@ router.get('/SendOTP', (req, res) => {
     });
 })
 
-router.post('/EditUser', isLoggedIn, async (req, res) => {
-    const params = {
-        UserPoolId: "ap-south-1_9ErMvHoXm",
-        Username: req.session.user.idToken.payload.email,
-        UserAttributes: [
-            {
-                Name: "email",
-                Value: req.body.Email
-            },
-            {
-                Name: "email_verified",
-                Value: "false"
-            },
-            {
-                Name: "name",
-                Value: req.body.Name
-            },
-            {
-                Name: "phone_number",
-                Value: req.body.Mobile
-            },
-            {
-                Name: "gender",
-                Value: req.body.Gender
-            },
-            {
-                Name: "custom:aadhar",
-                Value: req.body.Aadhar
-            },
-            {
-                Name: "address",
-                Value: req.body.Address
-            },
-            {
-                Name: "custom:city",
-                Value: req.body.City
-            },
-            {
-                Name: "custom:state",
-                Value: req.body.State
-            },
-            {
-                Name: "custom:country",
-                Value: req.body.Country
-            },
-            {
-                Name: "custom:pincode",
-                Value: req.body.Pincode
-            }
-        ],
-    };
-    const cognitoClient = new AWS.CognitoIdentityServiceProvider();
-    await cognitoClient.adminUpdateUserAttributes(params).promise().then(data => {
-        res.redirect("/Login");
-    }).catch(err => {
-        res.send(err);
-    });
-});
+
 
 module.exports = router;
--- routes/user/user.js
+++ routes/user/user.js
@@ -1,25 +1,69 @@
 const express = require('express');
-const isLoggedIn = require('../../middleware');
 const router = express.Router();
+const userRole = require('../../isUser');
 
-router.get('/ViewProfile',isLoggedIn,(req,res)=>{
+router.get('/ViewProfile',userRole.isUser,(req,res)=>{
     // console.log(req.session.user.idToken);
     res.render('user/viewprofile',{userData:req.session.user.idToken})
 })
 
-router.post('/UpdateUserProfile',isLoggedIn, (req, res) => {
-    var attributeList = [];
-    attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name:"name",Value:req.body.name}));
-    attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name:"preferred_username",Value:req.body.addhar}));
-    attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name:"phone_number",Value:req.body.phone}));
-    userPool.updateUserAttributes(req.session.user.idToken.payload.email, attributeList, function(err, result){
-        if (err) {
-            console.log(err);
-            return;
-        }
-        res.json(result);
-        console.log('user name is ' + result.user.getUsername());
+router.post('/EditUser', userRole.isUser, async (req, res) => {
+    const params = {
+        UserPoolId: "ap-south-1_9ErMvHoXm",
+        Username: req.session.user.idToken.payload.email,
+        UserAttributes: [
+            {
+                Name: "email",
+                Value: req.body.Email
+            },
+            {
+                Name: "email_verified",
+                Value: "false"
+            },
+            {
+                Name: "name",
+                Value: req.body.Name
+            },
+            {
+                Name: "phone_number",
+                Value: req.body.Mobile
+            },
+            {
+                Name: "gender",
+                Value: req.body.Gender
+            },
+            {
+                Name: "custom:aadhar",
+                Value: req.body.Aadhar
+            },
+            {
+                Name: "address",
+                Value: req.body.Address
+            },
+            {
+                Name: "custom:city",
+                Value: req.body.City
+            },
+            {
+                Name: "custom:state",
+                Value: req.body.State
+            },
+            {
+                Name: "custom:country",
+                Value: req.body.Country
+            },
+            {
+                Name: "custom:pincode",
+                Value: req.body.Pincode
+            }
+        ],
+    };
+    const cognitoClient = new AWS.CognitoIdentityServiceProvider();
+    await cognitoClient.adminUpdateUserAttributes(params).promise().then(data => {
+        res.redirect("/Login");
+    }).catch(err => {
+        res.send(err);
     });
-})
+});
 
 module.exports = router;
