99d36d508435f7af049842ff6fde884ad84112db
Pradyumna Gayake <55043418+prgayake@users.noreply.github.com>
2022-11-18 00:43:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -94,4 +94,4 @@ app.get('/ChangeUserPassword',userRole.isUser, (req, res) => {
     res.render('user/changePassword',{userData:req.session.user});
 })
 
-app.listen(process.env.PORT|| 3000, function () { console.log('Example app listening on port 3000!');});
+app.listen(process.env.PORT|| 3000,'40.117.125.57', function () { console.log('Example app listening on port 3000!');});
